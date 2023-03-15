d33bc668b1daba9cad8227e3e07966e0a885a972
beshan <beshan@pm.me>
2023-03-15 21:00:10 +0000

    v0.5.14

--- apps/sample/browser/src/main.ts
+++ apps/sample/browser/src/main.ts
@@ -3,7 +3,7 @@ import { concurrent } from '@bitair/concurrent.js'
 import type * as Services from './services/index.js'
 
 const main = async () => {
-  const { factorial } = await concurrent.module<typeof Services>('./services/index.js').load()
+  const { factorial } = await concurrent.import<typeof Services>('./services/index.js').load()
 
   const progress = setInterval(() => console.log('⯀'), 100) // Using this to show that the main thread is not blocked
 
